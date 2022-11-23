f52a0505779193b00e4f365ff5f5a647a74202ff
dambleerley <dambleerley@gmail.com>
2022-11-23 13:05:16 -0300

    Criando a view home

---
+++ Public/paginas/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stock - Login</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+</head>
+<body class="bg-dark">
+    <div class="container p-5">
+        <img src="assets/images/logo_white.png" class="mx-auto d-block" alt="Logo do sistema.">
+        <div class="card-body mt-5 mx-auto d-block" style="max-width:400px;">
+            <form action="" method="post">
+                <input type="email" placeholder="E-Mail" class="mt-3 form-control">
+                <input type="password" placeholder="****" class="mt-3 form-control">
+                <button type="submit" class="form-control btn btn-primary my-3">Entrar no sistema</button>
+                <a href="#"><i class="bi bi-exclamation-circle"></i> Equeci a minha senha.</a>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
