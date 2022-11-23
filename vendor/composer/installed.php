fa2d72d5230e3e396853f587fced78127313870d
dambleerley <dambleerley@gmail.com>
2022-11-23 07:50:10 -0300

    Adicionando o composer ao projeto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4ef8a4d934a45f7e74c3a53107a1d83a9a67b123',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4ef8a4d934a45f7e74c3a53107a1d83a9a67b123',
+            'dev_requirement' => false,
+        ),
+    ),
+);
