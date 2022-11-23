17d623087d151556ceb788bab4a929df542ecf2a
dambleerley <dambleerley@gmail.com>
2022-11-23 17:47:44 -0300

    Inserindo a lógica da classe home

--- App/Http/Home.php
+++ App/Http/Home.php
@@ -6,13 +6,13 @@ class Home
 {
     public function __construct()
     {
+        $banner = "Home Page";
         require_once 'Public/template/header.php';
-        require_once 'Public/paginas/home.php';
+        require_once 'Public/paginas/home.php';        
         require_once 'Public/template/footer.php';
     }
 
     public function home()
     {
-
     }
 }
